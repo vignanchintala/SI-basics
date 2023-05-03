23229ca4d42f6c3bad5ced967bfe1fbbf6ffccf7
vignanchintala <132432810+vignanchintala@users.noreply.github.com>
2023-05-03 12:14:08 +0530

    Update right-angled triangle pattern 2.py

--- right-angled triangle pattern 2.py
+++ right-angled triangle pattern 2.py
@@ -30,3 +30,25 @@ Explanation 0
 Self Explanatory
 '''
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n,count;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        count=i+1;
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d ",count);
+            count= count + n-j - 1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
